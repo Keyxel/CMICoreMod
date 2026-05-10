e4ef021ae9987527bb9256fd002b0a8d63167195
Qi-Month <dy757410@163.com>
2026-05-10 17:03:40 +0800

    lang

--- src/main/java/dev/celestiacraft/cmi/common/item/mechanism/StoneItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/item/mechanism/StoneItem.java
@@ -1,20 +1,12 @@
 package dev.celestiacraft.cmi.common.item.mechanism;
 
-import com.simibubi.create.AllItems;
-import dev.celestiacraft.cmi.Cmi;
 import dev.celestiacraft.cmi.common.item.MechanismItem;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 public class StoneItem extends MechanismItem {
 	public StoneItem(Properties properties) {
