aa8699289524cbdbdc1dab3d728d736671260066
xiaoliziawa <3682822069@qq.com>
2026-05-22 12:10:07 +0800

    feat(config): 添加堆叠存储配置选项

--- src/main/java/dev/celestiacraft/cmi/config/common/StackStorageConfig.java
+++ src/main/java/dev/celestiacraft/cmi/config/common/StackStorageConfig.java
@@ -4,12 +4,25 @@ import dev.celestiacraft.cmi.config.base.ConfigModule;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class StackStorageConfig extends ConfigModule {
+	public static ForgeConfigSpec.BooleanValue NO_STACK_UPGRADE_ENABLED;
+	public static ForgeConfigSpec.BooleanValue NO_STORAGE_STACK_ENABLED;
+
 	public StackStorageConfig(ForgeConfigSpec.Builder builder) {
-		super(builder, "stack_storage", "Stack Storage");
+		super(builder, "stack_storage", "Stack Storage settings");
 	}
 
 	@Override
 	protected void register() {
+		NO_STACK_UPGRADE_ENABLED = builder
+				.comment("Whether the cmi:no_stack_upgrade tag should disable storage stack scaling for matching items")
+				.comment("type: bool")
+				.comment("default: true")
+				.define("no_stack_upgrade_enabled", true);
 
+		NO_STORAGE_STACK_ENABLED = builder
+				.comment("Whether the cmi:no_storage_stack tag should force the in-storage stack limit of matching items to 1")
+				.comment("type: bool")
+				.comment("default: true")
+				.define("no_storage_stack_enabled", true);
 	}
 }
