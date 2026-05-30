691effe469312bc01ed13a16a488bd16e030116e
Qi-Month <dy757410@163.com>
2026-05-30 11:56:11 +0800

    WindVaneBlock

--- src/main/java/dev/celestiacraft/cmi/common/block/wind_vane/WindVaneBlock.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/wind_vane/WindVaneBlock.java
@@ -1,20 +1,12 @@
 package dev.celestiacraft.cmi.common.block.wind_vane;
 
+import com.simibubi.create.foundation.block.IBE;
 import dev.celestiacraft.cmi.common.register.CmiBlockEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.BaseEntityBlock;
+import dev.celestiacraft.libs.api.register.block.BasicBlock;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class WindVaneBlock extends BaseEntityBlock implements EntityBlock {
+public class WindVaneBlock extends BasicBlock implements IBE<WindVaneBlockEntity> {
 	public WindVaneBlock(Properties properties) {
 		super(Properties.copy(Blocks.COPPER_BLOCK)
 				.strength(3.0F, 6.0F)
@@ -22,19 +14,12 @@ public class WindVaneBlock extends BaseEntityBlock implements EntityBlock {
 	}
 
 	@Override
-	public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
-		return RenderShape.MODEL;
+	public Class<WindVaneBlockEntity> getBlockEntityClass() {
+		return WindVaneBlockEntity.class;
 	}
 
-	@Nullable
 	@Override
-	public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
-		return new WindVaneBlockEntity(CmiBlockEntity.WIND_VANE.get(), pos, state);
-	}
-
-	@Nullable
-	@Override
-	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
-		return null;
+	public BlockEntityType<? extends WindVaneBlockEntity> getBlockEntityType() {
+		return CmiBlockEntity.WIND_VANE.get();
 	}
 }
