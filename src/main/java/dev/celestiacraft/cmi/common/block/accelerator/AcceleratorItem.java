7378881c4ca6ac720fc654b6078bb3c26f7cfdd5
Qi-Month <dy757410@163.com>
2026-06-15 18:46:32 +0800

    修改AcceleratorItem继承逻辑

--- src/main/java/dev/celestiacraft/cmi/common/block/accelerator/AcceleratorItem.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/accelerator/AcceleratorItem.java
@@ -1,22 +1,10 @@
 package dev.celestiacraft.cmi.common.block.accelerator;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
+import dev.celestiacraft.libs.api.register.block.BasicBlockItem;
 import net.minecraft.world.level.block.Block;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
-public class AcceleratorItem extends BlockItem {
+public class AcceleratorItem extends BasicBlockItem {
 	public AcceleratorItem(Block block, Properties properties) {
 		super(block, properties);
-	}
-
-	@Override
-	public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
 	}
 }
